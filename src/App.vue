0cb4f7994181948901c76b5e0b80daddc4dd0b3d
andrew <andrew@DDI.com>
2021-03-18 18:24:58 +0800

    Andrew::完成应用的布局

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
-  <div style="width:240px;height:800px;background:red;"></div>
+  <!-- <div style="width:240px;height:800px;background:red;"></div>
   <Canvas style="width:600px;height:800px;background:green;"/>
   <Preview style="width:600px;height:800px;background:pink;" :htmlStr="htmlString" :cssString="cssString"></Preview>
-  <div style="width:300px;height:800px;background:orange;"></div>
+  <div style="width:300px;height:800px;background:orange;"></div> -->
+<div class="header">
+</div>
+<div class="container">
+  <div class="tree-container">
+
+  </div>
+  <div class="view-container">
+    <div class="content-box">
+
+    </div>
+  </div>
+  <div class="prop-container">
+
+  </div>
+</div>
 
 </template>
 
 <script>
-import beautify from "./tool/beautify.js";
-import Canvas from "./components/canvas/index.vue";
-import Preview from "./components/Preview.vue";
+// import beautify from "./tool/beautify.js";
+// import Canvas from "./components/canvas/index.vue";
+// import Preview from "./components/Preview.vue";
 export default {
   name: "App",
   components: {
-    Canvas,
-    Preview,
+    // Canvas,
+    // Preview,
   },
   data() {
     return {
@@ -23,32 +38,10 @@ export default {
     };
   },
   mounted() {
-    this.htmlString = beautify.html(`
-    <div>
-    哈哈哈哈
-      </div>
-    `);
-
-    beautify.css(
-      `
-    body{
-      color:red;
-      h1{
-        color:green;
-      }
-    }`,
-      (css) => {
-            this.cssString = css;
-      },
-    );
   },
 };
 </script>
 
 <style>
-#app{
-  display: flex;
-  justify-content: space-between;
-}
-
+@import url(./App.css);
 </style>
